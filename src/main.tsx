f6ad11bc9d6283b1dab770b55ec9b8ebe5e36ac7
AnsExp <rooseveltabrigo@gmail.com>
2025-11-06 17:35:18 -0500

    cambio pages 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import { createRoot } from 'react-dom/client'
-import AppRouter from './router/AppRouter.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage.tsx';
+import AboutPage from './pages/AboutPage.tsx';
+import ProjectsPage from './pages/ProjectsPage.tsx';
+import WorkWithMePage from './pages/WorkWithMePage.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <AppRouter />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/projects" element={<ProjectsPage />} />
+      <Route path="/work-with-me" element={<WorkWithMePage />} />
+    </Routes>
+  </BrowserRouter>
 )
